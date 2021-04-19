2346c1b835fec2eb9a884a2a06d6c50983117cce
keptoon <launcherstechinfo@gmail.com>
2021-04-19 21:04:04 +0530

    get wifi list nearby

--- lib/pages/User_page.dart
+++ lib/pages/User_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:wifi_iot/wifi_iot.dart';
 import 'package:worthy_net/pages/UserDetails_page.dart';
+import 'package:simply_wifi/simply_wifi.dart';
 
 class UserPage extends StatefulWidget {
   @override
@@ -7,26 +9,51 @@ class UserPage extends StatefulWidget {
 }
 
 class _UserPageState extends State<UserPage> {
+  List<WifiNetwork> wifiNetworks;
+  bool isLoading = true;
+  @override
+  void initState() {
+    super.initState();
+    SimplyWifi.init();
+    SimplyWifi.turnOnWifi();
+    getListOfWifiAvailable();
+  }
+
+  getListOfWifiAvailable() async {
+    // To get the list of Wifis
+    List<WifiNetwork> _wifiNetworks = await SimplyWifi.getListOfWifis();
+    setState(() {
+      wifiNetworks = _wifiNetworks;
+      isLoading = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          'List vew',
+          'List of all hosts',
         ),
       ),
-      body: buildListView(context),
+      body: isLoading
+          ? Center(child: Image.asset("assets/radar.gif"))
+          : buildListView(context, wifiNetworks),
     );
   }
 
-  ListView buildListView(BuildContext context) {
+  ListView buildListView(BuildContext context, List<WifiNetwork> listWifi) {
     return ListView.builder(
-      itemCount: 10,
+      itemCount: listWifi?.length,
       itemBuilder: (_, index) {
         return ListTile(
-          title: Text('Ther list item #$index'),
-          subtitle: Text('Sub title'),
-          leading: Icon(Icons.thumb_up),
+          title: Text(listWifi[index]?.ssid),
+          subtitle: Text(listWifi[index]?.bssid.toString()),
+          leading: Icon(
+            Icons.wifi,
+            color: Colors.blue,
+            size: 40.0,
+          ),
           trailing: Icon(Icons.arrow_forward),
           onTap: () {
             Navigator.push(
